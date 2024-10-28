a00182cd5c5a88589892f6384cba443b36461e61
KALYAN17408 <kalyanchandana926@gmail.com>
2024-10-28 12:30:25 +0530

    Create monthname.c

---
+++ monthname.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a number:");
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:printf("%d is january month",a);
+        break;
+        case 2:printf("%d is February",a);
+        break;
+        case 3:printf("%d is march month",a);
+        break;
+        case 4:printf("%d is aprilth",a);
+        break;
+        case 5:printf("%d is january month",a);
+        break;
+        case 6: printf("%d is january month",a);
+        break;
+        case 7:printf("%d is january month",a);
+        break;
+        case 8:printf("%d is january month",a);
+        break;
+        case 9:printf("%d is january month",a);
+        break;
+        case 10:printf("%d is january month",a);
+        break;
+        case 11:printf("%d is january month",a);
+        break;
+         case 12:printf("%d is january month",a);
+        break;
+        default:
+        printf("Invalid input");
+        break;
+    }
+}
