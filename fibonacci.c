111c4b3cc790d2bca9196f4d784bb60adedf0faf
KALYAN17408 <kalyanchandana926@gmail.com>
2024-12-23 12:08:26 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,20 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void main() {
+    int i,a,b,c,n;
+    printf("enter the value of n:");
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    printf("%d%d",a,b);
+    for(i=2;i<n;i++)
+    {
+        c=a+b;
+        printf("%d",c);
+        a=b;
+        b=c;
+    }
+    }
+    // Write C code here
+    
